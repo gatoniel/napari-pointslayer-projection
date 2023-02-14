f9f6bc884c8a0294e7956ab92c1ce29f9bfbdd67
Niklas Netter <niknett@gmail.com>
2023-02-14 11:12:30 +0100

    should work

--- src/napari_pointslayer_projection/__init__.py
+++ src/napari_pointslayer_projection/__init__.py
@@ -2,9 +2,6 @@ try:
     from ._version import version as __version__
 except ImportError:
     __version__ = "unknown"
-from ._widget import ExampleQWidget, example_magic_widget
+from ._widget import project_points
 
-__all__ = (
-    "ExampleQWidget",
-    "example_magic_widget",
-)
+__all__ = ("project_points",)
--- src/napari_pointslayer_projection/_widget.py
+++ src/napari_pointslayer_projection/_widget.py
@@ -8,39 +8,21 @@ Replace code below according to your needs.
 """
 from typing import TYPE_CHECKING
 
-from magicgui import magic_factory
-from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget
+from napari.layers import Points
 
 if TYPE_CHECKING:
     import napari
 
 
-class ExampleQWidget(QWidget):
-    # your QWidget.__init__ can optionally request the napari viewer instance
-    # in one of two ways:
-    # 1. use a parameter called `napari_viewer`, as done here
-    # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
-    def __init__(self, napari_viewer):
-        super().__init__()
-        self.viewer = napari_viewer
-
-        btn = QPushButton("Click me!")
-        btn.clicked.connect(self._on_click)
-
-        self.setLayout(QHBoxLayout())
-        self.layout().addWidget(btn)
-
-    def _on_click(self):
-        print("napari has", len(self.viewer.layers), "layers")
-
-
-@magic_factory
-def example_magic_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
-
-
-# Uses the `autogenerate: true` flag in the plugin manifest
-# to indicate it should be wrapped as a magicgui to autogenerate
-# a widget.
-def example_function_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
+def project_points(
+    points_data: "napari.types.PointsData",
+    face_color: str = "red",
+    edge_color: str = "red",
+) -> "napari.layers.Points":
+    return Points(
+        points_data[:, -2:],
+        name="projected points",
+        ndim=2,
+        face_color=face_color,
+        edge_color=edge_color,
+    )
